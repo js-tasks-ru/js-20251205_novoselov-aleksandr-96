a72cdd404ed03a04f03bc758fb14df1e5d590646
Novoselov Aleksandr <novoselov.aleksandr.96@mail.ru>
2026-01-14 23:20:32 +0500

    [master] отображение данных

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -40,22 +40,7 @@ export default class ColumnChart extends Component {
             <div class="column-chart__container">
               <div data-element="header" class="column-chart__header">${this.#value}</div>
               <div data-element="body" class="column-chart__chart">
-                <div style="--value: 12" data-tooltip="25%"></div>
-                <div style="--value: 16" data-tooltip="33%"></div>
-                <div style="--value: 8" data-tooltip="17%"></div>
-                <div style="--value: 33" data-tooltip="67%"></div>
-                <div style="--value: 14" data-tooltip="29%"></div>
-                <div style="--value: 6" data-tooltip="13%"></div>
-                <div style="--value: 25" data-tooltip="50%"></div>
-                <div style="--value: 8" data-tooltip="17%"></div>
-                <div style="--value: 6" data-tooltip="13%"></div>
-                <div style="--value: 18" data-tooltip="38%"></div>
-                <div style="--value: 50" data-tooltip="100%"></div>
-                <div style="--value: 41" data-tooltip="83%"></div>
-                <div style="--value: 35" data-tooltip="71%"></div>
-                <div style="--value: 41" data-tooltip="83%"></div>
-                <div style="--value: 4" data-tooltip="8%"></div>
-                <div style="--value: 2" data-tooltip="4%"></div>
+              ${this.#data.map(value => `<div style="--value: ${value}" data-tooltip="${value * 2}%"></div>`) .join('\n')}
               </div>
             </div>
           </div>
