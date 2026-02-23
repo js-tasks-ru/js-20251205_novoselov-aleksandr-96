720e922f435769d29fbcb1d72c1f31cec2bd8071
Novoselov Aleksandr <novoselov.aleksandr.96@mail.ru>
2026-02-23 13:18:57 +0500

    [master] product-form-v2 init

--- 09-tests-for-frontend-apps/1-product-form-v2/index.js
+++ 09-tests-for-frontend-apps/1-product-form-v2/index.js
@@ -1,13 +1,21 @@
 import SortableList from '../2-sortable-list/index.js';
 import escapeHtml from './utils/escape-html.js';
 import fetchJson from './utils/fetch-json.js';
+import { Component } from "../../components/component.js";
 
 const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ProductForm {
+export default class ProductForm extends Component {
   constructor (productId) {
+    super();
     this.productId = productId;
+
+    this.html = this.#template();
+  }
+
+  #template() {
+    return ``;
   }
 
   async render () {
