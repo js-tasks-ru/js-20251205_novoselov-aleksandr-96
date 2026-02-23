8a00ece00c03b0e565507e4ed875edfdb9d65922
Novoselov Aleksandr <novoselov.aleksandr.96@mail.ru>
2026-02-23 11:24:24 +0500

    [master] sortable-list should be rendered correctly

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -1,5 +1,19 @@
 import { Component } from "../../components/component.js";
 
 export default class SortableList extends Component {
+  constructor() {
+    super();
+
+    this.render();
+  }
+
+  #template() {
+    return `<div></div>`;
+  }
+
+  render() {
+    this.html = this.#template();
+  }
+    
 
 }
